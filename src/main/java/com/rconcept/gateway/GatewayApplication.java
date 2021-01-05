8b9211ebe04333dda0472490c1fe7b0d24e58e1f
LZx-space <L.mikespace@outlook.com>
2021-01-06 00:13:06 +0800

    gradle等此类基础文件

---
+++ src/main/java/com/rconcept/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.rconcept.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LZx
+ * @since 2020/12/31
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
